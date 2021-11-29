b301fd6bcbc8e033d3d27ca858b142dad94ccf9d
liuyunfz <957843164@qq.com>
2021-11-29 17:26:07 +0800

    修复了背景图片显示错误

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,37 +50,39 @@ $(document).ready(function () {
 	 * 改用 JsonBird：https://bird.ioliu.cn/
 	 * 
 	 */
-	var url = 'https://bird.ioliu.cn/v1/?url=https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
-	var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
-	var index = sessionStorage.getItem("index");
+	// var url = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8';
+	// var imgUrls = JSON.parse(sessionStorage.getItem("imgUrls"));
+	// var index = sessionStorage.getItem("index");
 	var $panel = $('#panel');
-	if(imgUrls == null){
-		imgUrls = new Array();
-		index = 0;		
-		$.get(url,function (result) {
-			images = result.images;
-			for (let i = 0; i < images.length; i++) {
-				const item = images[i];
-				imgUrls.push(item.url);
-			}
-			var imgUrl = imgUrls[index];
-			var url = "https://www.bing.com"+imgUrl;
-			$panel.css("background", "url('"+url+"') center center no-repeat #666");
-			$panel.css("background-size", "cover");
-			sessionStorage.setItem("imgUrls",JSON.stringify(imgUrls));
-			sessionStorage.setItem("index",index);
-			});
-	}else{
-		if(index == 7)
-			index = 0;
-		else
-			index++;
-		var imgUrl = imgUrls[index];
-		var url = "https://www.bing.com"+imgUrl;
-		$panel.css("background", "url('"+url+"') center center no-repeat #666");
-		$panel.css("background-size", "cover");
-		sessionStorage.setItem("index",index);
-	}
+	// if(imgUrls == null){
+	// 	imgUrls = new Array();
+	// 	index = 0;		
+	// 	$.get(url,function (result) {
+	// 		images = result.images;
+	// 		for (let i = 0; i < images.length; i++) {
+	// 			const item = images[i];
+	// 			imgUrls.push(item.url);
+	// 		}
+	// 		var imgUrl = imgUrls[index];
+	// 		var url = "https://cn.bing.com"+imgUrl;
+	// 		$panel.css("background", "url('"+url+"') center center no-repeat #666");
+	// 		$panel.css("background-size", "cover");
+	// 		sessionStorage.setItem("imgUrls",JSON.stringify(imgUrls));
+	// 		sessionStorage.setItem("index",index);
+	// 		});
+	// }else{
+	// 	if(index == 7)
+	// 		index = 0;
+	// 	else
+	// 		index++;
+	// 	var imgUrl = imgUrls[index];
+	// 	var url = "https://cn.bing.com"+imgUrl;
+	// 	$panel.css("background", "url('"+url+"') center center no-repeat #666");
+	// 	$panel.css("background-size", "cover");
+	// 	sessionStorage.setItem("index",index);
+	// }
+	$panel.css("background", "url('"+"https://bing.ioliu.cn/v1/rand"+"') center center no-repeat #666");
+	$panel.css("background-size", "cover");
 	
 	$(".iUp").each(function (i, e) {
 		iUp.up(e);
